687e06e8f629c1f1c97cbf10ce2133325314214b
jlhostyn <jlhostyn@ualberta.ca>
2016-03-09 17:26:41 -0700

    The project with gitignore implemented for visual basic so now only needed files will be on the repo

---
+++ Urban Planning Simulation/SurfaceWindow1.xaml.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Surface;
+using Microsoft.Surface.Presentation;
+using Microsoft.Surface.Presentation.Controls;
+using Microsoft.Surface.Presentation.Input;
+
+namespace Urban_Planning_Simulation
+{
+    /// <summary>
+    /// Interaction logic for SurfaceWindow1.xaml
+    /// </summary>
+    public partial class SurfaceWindow1 : SurfaceWindow
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public SurfaceWindow1()
+        {
+            InitializeComponent();
+
+            // Add handlers for window availability events
+            AddWindowAvailabilityHandlers();
+        }
+
+        /// <summary>
+        /// Occurs when the window is about to close. 
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            // Remove handlers for window availability events
+            RemoveWindowAvailabilityHandlers();
+        }
+
+        /// <summary>
+        /// Adds handlers for window availability events.
+        /// </summary>
+        private void AddWindowAvailabilityHandlers()
+        {
+            // Subscribe to surface window availability events
+            ApplicationServices.WindowInteractive += OnWindowInteractive;
+            ApplicationServices.WindowNoninteractive += OnWindowNoninteractive;
+            ApplicationServices.WindowUnavailable += OnWindowUnavailable;
+        }
+
+        /// <summary>
+        /// Removes handlers for window availability events.
+        /// </summary>
+        private void RemoveWindowAvailabilityHandlers()
+        {
+            // Unsubscribe from surface window availability events
+            ApplicationServices.WindowInteractive -= OnWindowInteractive;
+            ApplicationServices.WindowNoninteractive -= OnWindowNoninteractive;
+            ApplicationServices.WindowUnavailable -= OnWindowUnavailable;
+        }
+
+        /// <summary>
+        /// This is called when the user can interact with the application's window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowInteractive(object sender, EventArgs e)
+        {
+            //TODO: enable audio, animations here
+        }
+
+        /// <summary>
+        /// This is called when the user can see but not interact with the application's window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowNoninteractive(object sender, EventArgs e)
+        {
+            //TODO: Disable audio here if it is enabled
+
+            //TODO: optionally enable animations here
+        }
+
+        /// <summary>
+        /// This is called when the application's window is not visible or interactive.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowUnavailable(object sender, EventArgs e)
+        {
+            //TODO: disable audio, animations here
+        }
+
+        // Called when the start button is clicked. Go to the main window we'll be using
+        // (probably a better way to do this than creating a new window but for now it works)
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            SurfaceWindow2 mainWindow = new SurfaceWindow2();
+            mainWindow.Show();
+            // Don't need start window anymore
+            this.Close();
+        }
+    }
+}
