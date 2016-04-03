bcf90fe12e7479ea32b5e3d42b93dec1e49f6927
Justin Wu <justin.h.y.wu@gmail.com>
2016-04-03 10:39:35 -0600

    Add test mode coordinates

--- Urban Planning Simulation/TestModeScreen.xaml.cs
+++ Urban Planning Simulation/TestModeScreen.xaml.cs
@@ -21,10 +21,20 @@ namespace Urban_Planning_Simulation
     // Interaction logic for TestModeScreen.xaml
     public partial class TestModeScreen : SurfaceWindow
     {
+        private const bool DEBUG_MODE = true;
+
         // Default constructor.
         public TestModeScreen()
         {
+            // Set up layout
             InitializeComponent();
+
+            if (DEBUG_MODE)
+            {
+                TestModeDebugText.Visibility = Visibility.Visible;
+            }
+
+            createTestOne();
 
             // Add handlers for window availability events
             AddWindowAvailabilityHandlers();
@@ -75,6 +85,61 @@ namespace Urban_Planning_Simulation
         private void OnWindowUnavailable(object sender, EventArgs e)
         {
             //TODO: disable audio, animations here
+        }
+
+        //======================================================================
+        //                       TestCase Functions
+        //======================================================================
+        private void createTestOne()
+        {
+            // Create house ScatterViewItems in proper position
+            ScatterViewItem houseOne = new ScatterViewItem();
+            houseOne = SetSVHouseImage("HouseEMI");
+            houseOne.CanMove = false;
+            houseOne.CanScale = false;
+            houseOne.CanRotate = false;
+            houseOne.Center = new Point(200, 200);
+
+            TestScatterView.Items.Add(houseOne);
+        }
+
+        //======================================================================
+        //                       Helper Functions
+        //======================================================================
+        private ScatterViewItem SetSVHouseImage(String type)
+        {
+            ScatterViewItem item = new ScatterViewItem();
+            BitmapImage img = new BitmapImage();
+
+            if (type.Equals("HouseEMI", StringComparison.Ordinal))
+            {
+                img = new BitmapImage(new Uri("Resources/iso_house_1.png", UriKind.Relative));
+
+            }
+            else if (type.Equals("BuildingEMI", StringComparison.Ordinal))
+            {
+                img = new BitmapImage(new Uri("Resources/iso_building_1.png", UriKind.Relative));
+            }
+            else if (type.Equals("SkyscraperEMI", StringComparison.Ordinal))
+            {
+                img = new BitmapImage(new Uri("Resources/iso_skyscraper_1.png", UriKind.Relative));
+            }
+
+            ImageBrush imgBrush = new ImageBrush();
+            imgBrush.ImageSource = img;
+            item.Background = imgBrush;
+
+            return item;
+        }
+
+        // When in debug mode, the coordinates of mouse are shown in corner
+        private void TestScatterView_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (DEBUG_MODE)
+            {
+                Point location = e.GetPosition(this);
+                TestModeDebugText.Text = "(" + location.ToString() + ")";
+            }
         }
     }
 }
